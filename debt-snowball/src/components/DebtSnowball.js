8d739a9237f50c14066e0b07a13d32874ffbb13e
stark7991 <stark7991@gmail.com>
2024-07-20 01:24:58 -0400

    fixing math and statements

--- debt-snowball/src/components/DebtSnowball.js
+++ debt-snowball/src/components/DebtSnowball.js
@@ -7,6 +7,7 @@ function DebtSnowball({ debts }) {
     const sortedDebts = [...debts].sort((a, b) => a.balance - b.balance);
     const snowballPlan = [];
     let extraPayment = 0;
+    let currentMonth = new Date();
 
     sortedDebts.forEach((debt, index) => {
       let months = 0;
@@ -14,6 +15,7 @@ function DebtSnowball({ debts }) {
       let interestRate = debt.interestRate / 100 / 12;
       let monthlyPayment = debt.monthlyPayment + extraPayment;
 
+      // Calculate payoff time considering ongoing payments
       while (balance > 0) {
         balance += balance * interestRate; // Add monthly interest
         balance -= monthlyPayment; // Subtract monthly payment
@@ -26,7 +28,7 @@ function DebtSnowball({ debts }) {
       }
 
       // Calculate payoff date
-      const payoffDate = new Date();
+      const payoffDate = new Date(currentMonth);
       payoffDate.setMonth(payoffDate.getMonth() + months);
       const payoffMonth = payoffDate.toLocaleString('default', { month: 'long' });
       const payoffYear = payoffDate.getFullYear();
@@ -35,11 +37,12 @@ function DebtSnowball({ debts }) {
         ...debt,
         months,
         payoffDate: `${payoffMonth} ${payoffYear}`,
-        extraPayment: extraPayment ? `if you put an extra $${extraPayment.toFixed(2)}` : '',
       });
 
       // Update the extra payment to include the current debt's monthly payment
       extraPayment += debt.monthlyPayment;
+      // Move the currentMonth to the end of the current debt's payoff
+      currentMonth.setMonth(currentMonth.getMonth() + months);
     });
 
     return snowballPlan;
@@ -50,15 +53,15 @@ function DebtSnowball({ debts }) {
   return (
     <div>
       <h2 className="text-2xl font-bold mt-4">Debt Snowball Plan</h2>
-      <ul className="list-disc list-inside mt-2">
+      <ol className="list-decimal list-inside mt-2">
         {snowballPlan.map((debt, index) => (
           <li key={index}>
             {index === 0
-              ? `You will pay a minimum of $${debt.monthlyPayment.toFixed(2)} to ${debt.name} and this will be paid off in ${debt.payoffDate}.`
-              : `Now once ${snowballPlan[index - 1].name} is paid off, you will pay an extra $${snowballPlan[index - 1].monthlyPayment.toFixed(2)} to ${debt.name} which will be paid off in ${debt.payoffDate}.`}
+              ? ` You will pay a minimum of $${debt.monthlyPayment.toFixed(2)} to ${debt.name} and this will be paid off in ${debt.payoffDate}.`
+              : ` Now once ${snowballPlan[index - 1].name} is paid off, you will pay an extra $${snowballPlan[index - 1].monthlyPayment.toFixed(2)} to ${debt.name} which will be paid off in ${debt.payoffDate}.`}
           </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 }
